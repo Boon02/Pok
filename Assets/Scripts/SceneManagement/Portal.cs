763fdf47145cc7ad51f077f92b39e6c3f33c5a15
hvqUnityDev <hoangvanquan2002.unitydevelop@gmail.com>
2022-08-05 23:20:16 +0700

    Indoor Scene and Fading on Scene Switch

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,14 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
     [SerializeField] private Transform spawnPoint;
     
     private PlayerController player;
+
+    private Fader fader;
     
+    private void Start()
+    {
+        fader = FindObjectOfType<Fader>();
+    }
+
     public void OnPlayerTriggered(PlayerController player)
     {
         this.player = player;
@@ -23,12 +31,13 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         DontDestroyOnLoad(gameObject);
 
         GameController.Instance.PauseGame(true);
-
+        yield return fader.FaderIn(0.5f);
         yield return SceneManager.LoadSceneAsync(sceneNum);
 
         var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
         
+        yield return fader.FaderOut(0.5f);
         GameController.Instance.PauseGame(false);
     
         Destroy(gameObject);
